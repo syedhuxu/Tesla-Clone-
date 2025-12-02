5000ebd8bc41b1d14e85e4ac0ece07f6bb19aaaa
Syed Huzaif <syedhuzzu0198@gmail.com>
2025-12-02 22:42:03 +0530

    section 4 completed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//hero section image effect
 const heroSection = document.querySelector(".hero-section");
 const modelName = document.querySelector(".model-name");
 const images = [
@@ -20,3 +21,20 @@ setInterval(() => {
     heroSection.style.opacity = 1;
   }, 600);
 }, 3000);
+
+// video auto play and pause btn
+const videoDiv = document.querySelector(".video-div");
+const video = videoDiv.querySelector("video");
+const icon = videoDiv.querySelector(".icon i");
+
+videoDiv.querySelector(".icon").addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    icon.classList.remove("ri-play-large-fill");
+    icon.classList.add("ri-pause-large-fill");
+  } else {
+    video.pause();
+    icon.classList.remove("ri-pause-large-fill");
+    icon.classList.add("ri-play-large-fill");
+  }
+});
